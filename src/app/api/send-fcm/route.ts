0fc62446ea75cd52bb2971628f0d0bde2f4b47ef
후니 <hipark@huniui-MacBookPro.local>
2025-05-04 21:01:22 +0900

    Fix: FCM Test API

--- src/app/api/send-fcm/route.ts
+++ src/app/api/send-fcm/route.ts
@@ -17,7 +17,7 @@ interface NotificationData {
 const sendFCMNotification = async (data: NotificationData) => {
     // Firebase Admin SDK 초기화
     const serviceAccount: ServiceAccount = {
-        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     };
